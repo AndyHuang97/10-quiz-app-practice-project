a44d1b4822e6976aef18651cf2b4b2787b8570ee
Andrea Huang <huangandrea@hotmail.it>
2025-04-29 16:47:13 +0200

    Setting Different Timers Based On The Selected Answer

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -5,13 +5,21 @@ import Answers from "./Answers.jsx";
 
 import QUESTIONS from "../questions.js";
 
-const TIMEOUT = 10000;
-
 export default function Question({ index, onSelectAnswer, onSkipAnswer }) {
   const [answer, setAnswer] = useState({
     selectedAnswer: "",
     isCorrect: null,
   });
+
+  let timer = 10000;
+
+  if (answer.selectedAnswer) {
+    timer = 1000;
+  }
+
+  if (answer.isCorrect !== null) {
+    timer = 2000;
+  }
 
   function handleSelectAnswer(answer) {
     setAnswer({
@@ -27,7 +35,7 @@ export default function Question({ index, onSelectAnswer, onSkipAnswer }) {
 
       setTimeout(() => {
         onSelectAnswer(answer);
-      }, 1000)
+      }, 2000);
     }, 1000);
   }
 
@@ -36,12 +44,17 @@ export default function Question({ index, onSelectAnswer, onSkipAnswer }) {
   if (answer.selectedAnswer && answer.isCorrect !== null) {
     answerState = answer.isCorrect ? "correct" : "wrong";
   } else if (answer.selectedAnswer) {
-    answerState = "answered"
+    answerState = "answered";
   }
 
   return (
     <div id="question">
-      <QuestionTimer timeout={TIMEOUT} onTimeout={onSkipAnswer} />
+      <QuestionTimer
+        key={timer}
+        timeout={timer}
+        onTimeout={answer.selectedAnswer === "" ? onSkipAnswer : null}
+        mode={answerState}
+      />
       <h2>{QUESTIONS[index].text}</h2>
       <Answers
         answers={QUESTIONS[index].answers}
